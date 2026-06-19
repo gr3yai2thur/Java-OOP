2533e4a211d69c4f1484ebf8c6d01e5647a371ab
S i r H u s k y <bovonnatchaladdee@gmail.com>
2026-06-20 03:44:59 +0700

    SS d2 ex.1-10 L2

---
+++ src/DemoPerson.java
@@ -0,0 +1,28 @@
+public record DemoPerson() {
+    public static void main(String[] args) {
+        Person p1 = new Person("dome", 'M', "123-456-7890");
+        Person p2 = new Person("ken", 'M', "098-765-4321");
+        Person p3 = new Person("ploy", 'F', "555-555-5555");
+        Person p4 = new Person("may", 'F', "111-222-3333");
+        Person p5 = new Person("ann", 'F', "444-555-6666");
+        Person p6 = new Person("fon", 'F', "777-888-9999");
+
+        p1.gender = 'M';
+        p1.setTelno("123-456-7890");
+
+        p3.gender = 'F';
+        p3.setTelno("555-555-5555");
+
+
+        p1.setFan(p3);
+
+        p1.setGig(p4);
+        p1.setGig(p5);
+        p1.setGig(p6);
+        System.out.println(p1.getFan(p3));
+        System.out.println(p1.getGig(p1));
+
+        System.out.println(p1.getPersonInfo(p2));
+    }
+        
+}
---
+++ src/Person.java
@@ -0,0 +1,107 @@
+public class Person {
+    public String name;
+    public char gender;
+    private String telno;
+    public Person fan;
+    public Person[] gig = new Person[2];
+
+    Person(String name) {
+        this.name = name;
+    }
+
+    Person(String name, char gender, String telno) {
+        this.name = name;
+        this.gender = gender;
+        this.telno = telno;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void setTelno(String telno) {
+        this.telno = telno;
+    }
+
+    public String getPhone(Person p) {
+        if (p.gender != this.gender) {
+            return this.telno;
+        } else {
+            return "Access denied";
+        }
+    }
+
+    public String setFan(Person fan) {
+        if (fan.gender == this.gender) {
+            return "No gay.";
+        }
+        if (this.fan != null) {
+            return "Can't change fan now.";
+        }
+        this.fan = fan;
+        return null;
+    }
+
+    public String getFan(Person p) {
+        if (this.fan != null && this.fan.name.equals(p.name)) return "I luv u";
+        else return "I'm single";
+    }
+
+    public String setGig(Person g) {
+        for (int i = 0; i < gig.length; i++) {
+            if (gig[i] == null) {
+                gig[i] = g;
+                return null;
+            }
+        }
+        return "2 is enough";
+    }
+
+    public String getGig(Person p) {
+        if (p.gender == this.gender) {
+            String result = "";
+            for (int i = 0; i < gig.length; i++) {
+                if (gig[i] != null) {
+                    result += gig[i].name + " ";
+                }
+            }
+            return result;
+        } else {
+            return "No gigs";
+        }
+    }
+
+    public void removeGig(Person g) {
+        for (int i = 0; i < gig.length; i++) {
+            if (gig[i] == g) {
+                gig[i] = null;
+            }
+        }
+    }
+
+    public void removeGig() {
+        for (int i = 0; i < gig.length; i++) {
+            gig[i] = null;
+        }
+    }
+
+    public String getPersonInfo(Person requester) {
+        String fanName;
+        if (this.fan != null) {
+            fanName = this.fan.name;
+        } else {
+            fanName = "No fan";
+        }
+
+        String info = "=====================\n";
+        info += "Name: " + this.name + "\n";
+        info += "Gender: " + this.gender + "\n";
+        info += "Phone: " + this.getPhone(requester) + "\n";
+        info += "Fan: " + fanName + "\n";
+        info += "Gigs: " + this.getGig(requester) + "\n";
+        info += "=====================";
+
+        return info;
+    }
+}
